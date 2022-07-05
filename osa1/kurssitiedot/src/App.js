5022e3efa5afeca825b7c4c0e3c094dbe4df536b
Ville Häkli <ville@vhx.fi>
2022-07-05 14:38:04 +0300

    Refaktoroitu käyttämään taulukkoa

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,25 +1,27 @@
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   const Header = ({ course }) => <h1>{course}</h1>;
 
-  const Content = ({ part1, part2, part3 }) => (
+  const Content = ({ parts }) => (
     <>
-      <Part part={part1} />
-      <Part part={part2} />
-      <Part part={part3} />
+      {parts.map((part) => (
+        <Part part={part} />
+      ))}
     </>
   );
 
@@ -31,17 +33,18 @@ const App = () => {
     );
   };
 
-  const Total = ({ part1, part2, part3 }) => (
+  const Total = ({ parts }) => (
     <p>
-      Number of exercises {part1.exercises + part2.exercises + part3.exercises}
+      Number of exercises{" "}
+      {parts.reduce((total, part) => total + part.exercises, 0)}
     </p>
   );
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total part1={part1} part2={part2} part3={part3} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
