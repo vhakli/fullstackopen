81ee3739ea4f8e21d8b00bb9076ef06b8e483897
Ville Häkli <ville@vhx.fi>
2022-12-27 15:49:27 +0200

    2.6: puhelinluetteloon nimien lisäys ja listaus

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setPersons((persons) => [...persons, { name: newName }]);
+    setNewName("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name:{" "}
+          <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
