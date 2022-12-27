1bc3a1ae65122aefbb18c46fa826c324dcb86372
Ville Häkli <ville@vhx.fi>
2022-12-27 15:39:06 +0200

    2.5: course komponentti omaksi moduuliksi

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,33 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>;
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((s, p) => s + p.exercises, 0);
-  return <strong>total of {total} exercises</strong>;
-};
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-);
-
-const Content = ({ parts }) => (
-  <>
-    {parts.map((part) => (
-      <Part key={part.id} part={part} />
-    ))}
-  </>
-);
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
-};
+import { Course } from "./Course";
 
 const App = () => {
   const courses = [
